ba5194183c8e8c61ec5d7567727a956b0e3cc060
Asura <zetyt99@gmail.com>
2025-02-17 10:00:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const input = document.getElementById("terminal-input");
+
+    input.addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            const command = input.value.trim().toLowerCase();
+
+            // Pārbaudām komandu un atveram atbilstošo lapu
+            if (command === 'sudo cd open page-1') {
+                window.location.href = "page-1.html";
+            } else if (command === 'sudo cd open page-2') {
+                window.location.href = "page-2.html";
+            } else if (command === 'sudo cd open page-3') {
+                window.location.href = "page-3.html";
+            } else if (command === 'sudo cd open page-4') {
+                window.location.href = "page-4.html";
+            } else if (command === 'sudo cd open page-5') {
+                window.location.href = "page-5.html";
+            } else if (command === 'sudo cd open page-6') {
+                window.location.href = "page-6.html";
+            } else if (command === 'sudo cd open page-gallery') {
+                window.location.href = "gallery.html";
+            } else if (command === 'sudo cd open page-home') {
+                window.location.href = "virus-code-red.html";
+            }   else {
+                alert("Unknown command! Try: sudo cd open page-1");
+            }
+
+            input.value = ""; // Notīra ievades lauku
+        }
+    });
+});
